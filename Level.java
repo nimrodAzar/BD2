2b99b523d69691bc3b3e24e150c48ecb498a154e
nimrodAzar <156137602+nimrodAzar@users.noreply.github.com>
2024-06-05 12:40:43 +0300

    Update Level.java

--- Level.java
+++ Level.java
@@ -1,13 +1,21 @@
 package co.il.bulletdance;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+
 class Level{
     private List<Projectile> sequence;
     private List<Double> timer;
 
-    public Level(Projectile firstProj){}
+    public Level(){}
     public void Shoot(Projectile proj, double time){
         this.sequence.add(proj);
         this.timer.add(time);
     }
+    public List<Projectile> GetSeq(){return this.sequence;}
 }
