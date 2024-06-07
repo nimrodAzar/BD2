05a8a63e6602fbb4a9707537db58194e4b783254
nimrodAzar <156137602+nimrodAzar@users.noreply.github.com>
2024-06-07 13:14:26 +0300

    Update MainBoard.java

--- MainBoard.java
+++ MainBoard.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MainBoard extends View{
     int x;
@@ -45,13 +47,38 @@ public class MainBoard extends View{
         //Log.d("I2", Double.toString(Seq.get(0).GetPosX()));
     }
 
+    public MainBoard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        BG = BitmapFactory.decodeResource(getResources(),R.drawable.board_no_bg_2_40);
+
+
+
+        p = new Paint();
+        p.setColor(Color.RED);
+        p.setStrokeWidth(15);
+        x = 1;y = 1;
+        pla = new Projectile(10,60,true,x,y);
+        //Log.d("I",Double.toString(pla.GetPosX()));
+        lvl = new Level();
+        lvl.Shoot(pla,0);
+        lvl.Shoot(new Projectile(10,300,true,x,1000-y), 1);
+        Seq = lvl.GetSeq();
+        //Log.d("I3", Integer.toString(Seq.size()));
+        //Log.d("I2", Double.toString(Seq.get(0).GetPosX()));
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
+        Log.d("CANVAS", "onDraw: " + canvas.getWidth() +", " + canvas.getHeight());
+
         if (Seq.isEmpty()){
             lvl.MakeSeq1(canvas);
 
-            //Log.i("I10",Integer.toString(Seq.size()));
+            Log.i("I10",Integer.toString(Seq.size()));
         }
 
         Seq = lvl.GetSeq();
@@ -61,7 +88,8 @@ public class MainBoard extends View{
         for (int i = 0; i <Seq.size(); i++){
            //Log.d("I6",Integer.toString(i) + " " + Integer.toString(Seq.size()));
             DrawProjectile(canvas, Seq.get(i));
-            if(Seq.get(i).GetPosY()>canvas.getHeight() || Seq.get(i).GetPosX()>canvas.getWidth()) {
+            if(Seq.get(i).GetPosY()>canvas.getHeight() || Seq.get(i).GetPosX()>canvas.getWidth()
+            || Seq.get(i).GetPosY()<1 || Seq.get(i).GetPosX()<1) {
                 Seq.remove(i);
                 //Log.d("I8","RESET" + Integer.toString(i));
                 i = 0;
